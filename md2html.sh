7dc4128973ff9f3f3dbf9f033ff19fa97c162872
overmind1980 <easyo@easydeMBP.lan>
2022-10-08 22:12:27 +0800

    yes

--- md2html.sh
+++ md2html.sh
@@ -4,3 +4,4 @@ cd html
 find . -type f -name "*.md" | sed "s/\(\.\/\)\(.*\)\(.sy.md\)/\2/g" | xargs mkdir
 find . -type f -name "*.md" | sed "s/\(\.\/\)\(.*\)\(.sy.md\)/mv \1\2\3 \2/g" > go.sh
 chmod u+x go.sh
+
